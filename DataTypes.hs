fc604bc4907e66b46e3a7acc5c102d22d8bd03e4
Rachel Xu <rachel960122@hotmail.com>
2017-04-21 16:30:46 -0400

    readCamera and readLights

--- DataTypes.hs
+++ DataTypes.hs
@@ -8,20 +8,29 @@ type Color = Vec3
 data Vec3 = Vec3 Double Double Double
   deriving Show
 data Vec4 = Vec4 Double Double Double Double
+  deriving Show
 
 -- Sphere Vec3 (center) Double (radius) | Plane Vec4 (coefficients)
 data Object = Sphere Vec3 Double | Plane Vec4
+  deriving Show
 -- Camera Point (camera pos) Point (at point) Vec3 (up Vec3tor) Double (fovy)
 data Camera = Camera Point Point Vec3 Double
+  deriving Show
 -- Image Int (width) Int (height)
 data Image = Image Int Int
+  deriving Show
 -- Light Point (source) Color (intensity) Vec3c3 (attenuation)
 data Light = Light Point Color Vec3
+  deriving Show
 -- Ray Point (origin) Vec3 (direction)
 data Ray = Ray Point Vec3
+  deriving Show
 -- Solid Color (RGB components) | CheckerBoard Color (color1) Color (color2) Double (square size)
 data Pigment = Solid Color | CheckerBoard Color Color Double
+  deriving Show
 -- PhongCoef Double (ambient coef) Double (diffuse coef) Double (specular coef) Double (shininess)
 data PhongCoef = PhongCoef Double Double Double Double
+  deriving Show
 -- Surface PhongCoef Double (reflectivity coef) Double (transmission coef) Double (index of refraction)
 data Surface = Surface PhongCoef Double Double Double
+  deriving Show
--- Rachel.hs
+++ Rachel.hs
@@ -21,14 +21,41 @@ readPigments = error "Not Implemented"
 readSurfaces :: String -> [Surface]
 readSurfaces = error "Not Implemented"
 
-readLights :: String -> [Light]
-readLights = error "Not Implemented"
+readLights :: [String] -> [Light]
+readLights [] = []
+readLights (x : xs) = light : readLights xs where
+  vecs = strToVec3s x
+  light = Light (vecs !! 0) (vecs !! 1) (vecs !! 2)
 
 readImage :: String -> Image
 readImage = error "Not Implemented"
 
-readCamera :: String -> Camera
-readCamera = error "Not Implemented"
+readCamera :: [String] -> Camera
+readCamera (camera : at : up : fovy : []) = Camera c a u f where
+  c = head $ strToVec3s camera
+  a = head $ strToVec3s at
+  u = head $ strToVec3s up
+  f = read fovy :: Double
+readCamera _ = error "camera data corrupted"
+
+strToVec3s :: String -> [Vec3]
+strToVec3s = groupDoubles . strToDoubles
+
+-- "1.1 2.2" -> [1.1, 2.2]
+strToDoubles :: String -> [Double]
+strToDoubles str = map read (words str) :: [Double]
+
+-- [1.1, 2.2, 3.3, 4.4, 5.5, 6.6] -> [(Vec3 1.1 2.2 3.3), (Vec3 4.4, 5.5, 6.6)]
+groupDoubles :: [Double] -> [Vec3]
+groupDoubles ds
+  | length ds >= 3 = doublesToVec3 (take 3 ds) : (groupDoubles (drop 3 ds))
+  | otherwise = []
+
+-- [1.1, 2.2, 3.3] -> Vec3 1.1 2.2 3.3
+doublesToVec3 :: [Double] -> Vec3
+doublesToVec3 ds
+  | length ds == 3 = Vec3 (ds !! 0) (ds !! 1) (ds !! 2)
+  | otherwise = error "list length is not 3"
 
 -- Given ray, a specific object, calculate the intersection distance from ray origin in view coordinates.
 getIntersect :: Ray -> Object -> Double
@@ -38,6 +65,9 @@ getIntersect = error "Not Implemented"
 getNormal :: Object -> Point -> Vec3
 getNormal = error "Not Implemented"
 
+
+-- ========================================================================
+-- TODO: Change function names to match the actual arguments
 -- Given the matrix(2D array) of image_data, produce image in PPM P6 format
 write_ppm6 :: String -> Image -> M.Matrix Color -> IO()
 write_ppm6 str img mat = writePPM str img (doublesToWords (matrixToList mat))
@@ -77,7 +107,7 @@ dToB :: Double -> BIN.ByteString
 dToB d = BIN.fromChunks [DBC8.pack (show d)]
 
 fileName = "haha.ppm"
-size = 10
+size = 1000
 image = Image size size
 mat = M.fromList size size (replicate (size * size) (Vec3 255 0 0))
 mat2 = writePixel mat 1 1 (Vec3 0 255 0)
