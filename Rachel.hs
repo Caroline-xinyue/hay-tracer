dd3e0d549ef29e34f76fcd98dccf15ac7e6f738e
Rachel Xu <rachel960122@hotmail.com>
2017-04-21 04:09:43 -0400

    Got write_ppm to work (for P3)

--- Rachel.hs
+++ Rachel.hs
@@ -1,15 +1,15 @@
 module Rachel where
 
+import Prelude
 import DataTypes
+import Data.Word
+import System.IO
 import qualified Data.Vector as V
 import qualified Data.Matrix as M
-import Data.Word
 import qualified Data.ByteString.Lazy as BIN
-import qualified Data.ByteString.Conversion as DBC
 import qualified Data.ByteString.Char8 as DBC8
+import qualified Data.ByteString.Conversion as DBC
 import qualified Data.Double.Conversion.ByteString as DD
-import System.IO
-import Prelude
 
 -- Read input file into various GADTs
 readObjects :: String -> [Object]
@@ -39,16 +39,20 @@ getNormal :: Object -> Point -> Vec3
 getNormal = error "Not Implemented"
 
 -- Given the matrix(2D array) of image_data, produce image in PPM P6 format
-write_ppm6 :: String -> Image -> M.Matrix Vec3 -> IO()
+write_ppm6 :: String -> Image -> M.Matrix Color -> IO()
 write_ppm6 str img mat = writePPM str img (doublesToWords (matrixToList mat))
 
-stringPPM :: Image -> [(Word8,Word8,Word8)] -> BIN.ByteString
+stringPPM :: Image -> [(BIN.ByteString,BIN.ByteString,BIN.ByteString)] -> BIN.ByteString
 stringPPM image ps =
-  BIN.pack (map (fromIntegral . fromEnum) $ "P6\n" ++ show (getWidth image) ++ " " ++ show (getHeight image) ++ "\n255\n") `BIN.append`
-  BIN.concat (map (\(r,g,b) -> BIN.pack [r,g,b]) ps)
+  BIN.pack (map (fromIntegral . fromEnum) $ "P3\n" ++ show (getWidth image) ++ " " ++ show (getHeight image) ++ "\n255\n") `BIN.append`
+  BIN.concat (map (\(r,g,b) -> BIN.concat [r, space, g, space, b, space]) ps) where
+    space = BIN.pack [0x20]
 
-writePPM :: String -> Image -> [(Word8,Word8,Word8)] -> IO ()
+writePPM :: String -> Image -> [(BIN.ByteString,BIN.ByteString,BIN.ByteString)] -> IO ()
 writePPM f sz ps = BIN.writeFile f (stringPPM sz ps)
+
+writePixel :: M.Matrix Color -> Int -> Int -> Color -> M.Matrix Color
+writePixel mat x y c = M.setElem c (x, y) mat
 
 getWidth :: Image -> Int
 getWidth (Image x y) = x
@@ -56,7 +60,7 @@ getWidth (Image x y) = x
 getHeight :: Image -> Int
 getHeight (Image x y) = y
 
-matrixToList :: M.Matrix Vec3 -> [(Double, Double, Double)]
+matrixToList :: M.Matrix Color -> [(Double, Double, Double)]
 matrixToList m = concatMap vec3ListToTuple (M.toLists m)
 
 vec3ListToTuple :: [Vec3] -> [(Double, Double, Double)]
@@ -66,12 +70,14 @@ vec3ListToTuple (x : xs) = vec3ToTuple x : vec3ListToTuple xs
 vec3ToTuple :: Vec3 -> (Double, Double, Double)
 vec3ToTuple (Vec3 x y z) = (x, y, z)
 
-doublesToWords :: [(Double, Double, Double)] -> [(Word8,Word8,Word8)]
-doublesToWords ds = map (\(d1, d2, d3) -> (dToW d1, dToW d2, dToW d3)) ds
+doublesToWords :: [(Double, Double, Double)] -> [(BIN.ByteString,BIN.ByteString,BIN.ByteString)]
+doublesToWords ds = map (\(d1, d2, d3) -> (dToB d1, dToB d2, dToB d3)) ds
 
-dToW :: Double -> Word8
-dToW d = fromIntegral (round d) :: Word8
+dToB :: Double -> BIN.ByteString
+dToB d = BIN.fromChunks [DBC8.pack (show d)]
 
 fileName = "haha.ppm"
-image = Image 10 10
-mat = M.fromList 10 10 (replicate 100 (Vec3 10 10 10))
+size = 10
+image = Image size size
+mat = M.fromList size size (replicate (size * size) (Vec3 255 0 0))
+mat2 = writePixel mat 1 1 (Vec3 0 255 0)
