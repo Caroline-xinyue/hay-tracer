1b6cd4beca2a8a8becb3c0d356bbd9131b470668
Rachel Xu <rachel960122@hotmail.com>
2017-04-22 22:08:11 -0400

    Switch to record syntax

--- DataTypes.hs
+++ DataTypes.hs
@@ -16,8 +16,8 @@ data Vector3 a = Vector3 a a a
   deriving Show
 
 -- Sphere Vec3 (center) Double (radius) Int (np) Int (nf) | Plane Vec4 (coefficients)
-data Object = Sphere Vec3 Double Int Int | Plane Vec4 Int Int
-  deriving Show
+data Object = Sphere { center :: Vec3, radius :: Double, np :: Int, nf :: Int }
+            | Plane { coef :: Vec4, np :: Int, nf :: Int} deriving Show
 -- Camera Point (camera pos) Point (at point) Vec3 (up Vec3tor) Double (fovy)
 data Camera = Camera Point Point Vec3 Double
   deriving Show
@@ -34,8 +34,14 @@ data Ray = Ray Point Vec3
 data Pigment = Solid Color | CheckerBoard Color Color Double
   deriving Show
 -- PhongCoef Double (ambient coef) Double (diffuse coef) Double (specular coef) Double (shininess)
-data PhongCoef = PhongCoef Double Double Double Double
-  deriving Show
+data PhongCoef = PhongCoef { ka :: Double
+                           , kd :: Double
+                           , ks :: Double
+                           , alpha :: Double
+                         } deriving Show
 -- Surface PhongCoef Double (reflectivity coef) Double (transmission coef) Double (index of refraction)
-data Surface = Surface PhongCoef Double Double Double
-  deriving Show
+data Surface = Surface { phongCoef :: PhongCoef
+                       , kr :: Double
+                       , kt :: Double
+                       , ki :: Double
+                     } deriving Show
